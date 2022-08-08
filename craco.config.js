2134ee8437ad7e6651a85aac0b447b35c53e9ee2
Pavel Shapov <pavel.shapov@metalamp.io>
2022-08-08 19:50:29 +0700

    Change css-modules file resolution. Add type definition generation for modules

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,9 @@
 const CracoAlias = require('craco-alias');
 
 module.exports = {
+  webpack: {
+    configure: configureCSSModules,
+  },
   plugins: [
     {
       plugin: CracoAlias,
@@ -12,3 +15,32 @@ module.exports = {
     },
   ],
 };
+
+const cssRegex = /\.css$/;
+const cssModuleRegex = /\.module\.css$/;
+const sassRegex = /\.(scss|sass)$/;
+const sassModuleRegex = /\.module\.(scss|sass)$/;
+
+function configureCSSModules(webpackConfig) {
+  webpackConfig.module.rules = webpackConfig.module.rules.map((rule) => {
+    if (!Array.isArray(rule.oneOf)) return rule;
+
+    rule.oneOf = rule.oneOf
+      .map((loader) => {
+        if (String(loader.test) === String(cssRegex) || String(loader.test) === String(sassRegex)) {
+          return null;
+        }
+
+        if (String(loader.test) === String(cssModuleRegex)) loader.test = cssRegex;
+
+        if (String(loader.test) === String(sassModuleRegex)) loader.test = sassRegex;
+
+        return loader;
+      })
+      .filter(Boolean);
+
+    return rule;
+  });
+
+  return webpackConfig;
+}
