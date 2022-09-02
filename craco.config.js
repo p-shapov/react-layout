7f26d91522b80d7f375a80a1264e73101c9609df
Pavel Shapov <pavel.shapov@metalamp.io>
2022-09-02 12:58:33 +0700

    Update module resolution for scss

--- craco.config.js
+++ craco.config.js
@@ -2,9 +2,6 @@ const { CracoAliasPlugin } = require('react-app-alias');
 const SassResourcesLoader = require('craco-sass-resources-loader');
 
 module.exports = {
-  webpack: {
-    configure: configureCSSModules,
-  },
   plugins: [
     {
       plugin: SassResourcesLoader,
@@ -21,32 +18,3 @@ module.exports = {
     },
   ],
 };
-
-const cssRegex = /\.css$/;
-const cssModuleRegex = /\.module\.css$/;
-const sassRegex = /\.(scss|sass)$/;
-const sassModuleRegex = /\.module\.(scss|sass)$/;
-
-function configureCSSModules(webpackConfig) {
-  webpackConfig.module.rules = webpackConfig.module.rules.map((rule) => {
-    if (!Array.isArray(rule.oneOf)) return rule;
-
-    rule.oneOf = rule.oneOf
-      .map((loader) => {
-        if (String(loader.test) === String(cssRegex) || String(loader.test) === String(sassRegex)) {
-          return null;
-        }
-
-        if (String(loader.test) === String(cssModuleRegex)) loader.test = cssRegex;
-
-        if (String(loader.test) === String(sassModuleRegex)) loader.test = sassRegex;
-
-        return loader;
-      })
-      .filter(Boolean);
-
-    return rule;
-  });
-
-  return webpackConfig;
-}
